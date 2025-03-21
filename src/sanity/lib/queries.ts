49ff130ae68a4cf7bce1001f244b5015d306c16c
Majid Ali <160537411+Majidali80@users.noreply.github.com>
2025-03-21 07:40:21 +0500

    Update queries.ts

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -5,8 +5,15 @@ export const Query = groq`*[_type == "product"]`;
 export const productByIdQuery = groq`*[_type == "product" && _id == $id][0]`;
 export const productsByCategoryQuery = groq`*[_type == "product" && category == $category]`;
 
-export const allProductsQuery = groq`*[_type == "product"]{
-    _id, title, price, image, slug, discountPercentage, productName
+export const allProductsQuery = groq`*[_type == "product"] {
+  _id,
+  productName,
+  title,
+  "image": { "asset": { "url": image.asset->url } },
+  price,
+  discountPercentage,
+  slug,
+  reviews
   }`;
 
   
